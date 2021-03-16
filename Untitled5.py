6bed42ba91d827a0e0e47440ebc1c9bc3fe1a892
yernur003 <78781679+yernur003@users.noreply.github.com>
2021-03-16 13:03:55 +0600

    Add files via upload

---
+++ Untitled5.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+n=4
+for i in range(n):
+    for j in range(i):
+        print('#',end="")
+    print('')
+for i in range(n,0,-1):
+    for j in range(i):
+        print('#',end="")
+    print('')
+
+
+# In[ ]:
+
+
+
+
